8b1e096665ec3425929a389b348b00ba12aca338
Ryan Tate <ryan.michael.tate@gmail.com>
2017-07-17 12:13:26 -0700

    Updating web3 provider to select metamask when configured for true

--- src/web3provider.js
+++ src/web3provider.js
@@ -1,10 +1,14 @@
 import Web3 from 'web3'
 import { web3Provider, useMetaMask } from '../app.config'
 
-if (typeof web3 !== 'undefined' && useMetaMask == true) {
-  window.web3 = new Web3(web3.currentProvider)
-} else {
-  window.web3 = new Web3(new Web3.providers.HttpProvider(web3Provider))
+function configureWeb3() {
+  if (typeof window.web3 !== 'undefined' && useMetaMask == true) {
+    return new Web3(window.web3.currentProvider)
+  } else {
+    return new Web3(new Web3.providers.HttpProvider(web3Provider))
+  }
 }
 
-export default window.web3
+const web3 = configureWeb3()
+
+export default web3
