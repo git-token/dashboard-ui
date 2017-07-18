e8c6d5ea27d6daadd5c74ec14c946f5c83658ac6
Ryan Tate <ryan.michael.tate@gmail.com>
2017-07-18 09:49:53 -0700

    Temporarily debugging web3 race condition

--- src/actions/DashboardActions.js
+++ src/actions/DashboardActions.js
@@ -2,11 +2,12 @@ import Promise, { delay, promisifyAll } from 'bluebird'
 import GitTokenContract from 'gittoken-contracts/build/contracts/GitToken.json'
 import { w3cwebsocket } from 'websocket'
 import axios from 'axios'
+import Web3 from 'web3'
 
 import { initializeContract } from './ContractActions'
 import { socketServer } from '../../app.config'
-import web3 from '../web3Provider'
-
+// import web3 from '../web3Provider'
+const web3 = new Web3(window.web3.currentProvider)
 const eth = promisifyAll(web3.eth)
 
 
