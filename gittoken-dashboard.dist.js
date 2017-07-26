843bf13a863aee5668549b6e9ce7905696fe1992
Ryan Tate <ryan.michael.tate@gmail.com>
2017-07-26 13:51:11 -0700

    Automated msg

--- gittoken-dashboard.dist.js
+++ gittoken-dashboard.dist.js
@@ -50901,7 +50901,7 @@ module.exports = Cancel;
 module.exports = {
   web3Provider: "https://gittoken.org/web3/",
   useMetaMask: true,
-  socketServer: 'ws://127.0.0.1:1325',
+  socketServer: 'wss://gittoken.org/ws/',
   contractAddress: '0xedc2fe67c31c2aca5cf5866b2208fe1ec113e8c8'
 };
 
