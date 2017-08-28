ec5bf95720b1872e86f00b800a7c31ee1e2b5379
Ryan Tate <ryan.michael.tate@gmail.com>
2017-08-27 18:57:02 -0700

    Updating service provider configurations

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  web3Provider: "https://gittoken.org/web3/",
+  web3Provider: "https://web3.gittoken.io",
   useMetaMask: true,
-  socketServer: 'wss://gittoken.org/ws/',
+  socketServer: 'wss://socket.gittoken.io',
   contractAddress: '0xedc2fe67c31c2aca5cf5866b2208fe1ec113e8c8'
 }
